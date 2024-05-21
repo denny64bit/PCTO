d76e4c7bb9f47a586f4dd28359ae11999e81e379
Yassine <info@giaflix.it>
2024-05-21 12:22:05 +0200

    inserimento navbar laterale

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 document.getElementById('navbar').addEventListener('click', function () {
     var navBar = document.getElementById('nav-bar');
-    var everything = document.getElementsByTagName('body'); // Prendi il primo elemento di 'body'
+    var overlay = document.querySelector('.body-dark-overlay');
 
-    // Toggle tra l'aggiunta e la rimozione delle classi 'visible' e 'hidden' per mostrare o nascondere il pannello laterale e l'overlay
-    if (navBar.classList.contains('hidden')) {
-        navBar.classList.remove('hidden');
-        navBar.classList.add('visible');
-        everything.classList.add('body-dark-overlay'); // Aggiungi la classe per scurire il 'body'
-        navBar.classList.remove('body-dark-overlay'); // Aggiungi la classe per scurire il 'body'
+    // Se l'overlay non esiste, crealo e aggiungilo al body
+    if (!overlay) {
+        overlay = document.createElement('div');
+        overlay.className = 'body-dark-overlay';
+        document.body.appendChild(overlay);
+    }
 
-        console.log(everything);
-    } else {
+    // Funzione per toggle delle classi
+    function toggleClass(element, class1, class2) {
+        if (element.classList.contains(class1)) {
+            element.classList.remove(class1);
+            element.classList.add(class2);
+        } else {
+            element.classList.remove(class2);
+            element.classList.add(class1);
+        }
+    }
+
+    // Toggle delle classi per navBar e overlay
+    toggleClass(navBar, 'hidden', 'visible');
+    toggleClass(overlay, 'visible', 'hidden');
+});
+
+
+
+document.addEventListener('click', function (event) {
+    var overlay = document.querySelector('.body-dark-overlay');
+    var navBar = document.getElementById('nav-bar');
+
+    if (overlay && event.target === overlay) {
         navBar.classList.remove('visible');
         navBar.classList.add('hidden');
-        everything.classList.remove('body-dark-overlay'); // Rimuovi la classe per scurire il 'body'
+        overlay.classList.remove('visible');
+        overlay.classList.add('hidden');
     }
+});
+
+
+document.getElementById('close-navbar').addEventListener('click', function () {
+    var navBar = document.getElementById('nav-bar');
+    var overlay = document.querySelector('.body-dark-overlay');
+    var closeMenuIcon = document.getElementById('close-menu-icon');
+    var openMenuIcon = document.getElementById('open-menu-icon');
+
+    navBar.classList.remove('visible');
+    navBar.classList.add('hidden');
+    overlay.classList.remove('visible');
+    overlay.classList.add('hidden');
+    closeMenuIcon.classList.remove('visible');
+    closeMenuIcon.classList.add('hidden');
+    openMenuIcon.classList.remove('hidden');
+    openMenuIcon.classList.add('visible');
 });
